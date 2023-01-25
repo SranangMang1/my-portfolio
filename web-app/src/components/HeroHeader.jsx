25bd733461b91b6f7836104515f41e18846e60d9
SranangMang1 <kuonta@hotmail.com>
2023-01-25 14:35:10 -0500

    update to hero banner

--- web-app/src/components/HeroHeader.jsx
+++ web-app/src/components/HeroHeader.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { Paper } from "@mui/material";
-// import { makeStyles } from "tss-react/mui";
 import { makeStyles } from "@mui/styles";
-// import logo from "../src/logo.svg";
-// web-app\src\logo.svg
 
 const useStyles = makeStyles((theme) => ({
   section: {
     height: "100vh",
-    // backgroundImage: "(https://tinyurl.com/2p89cnxu)",
     backgroundImage: "url(/images/CoffeeCupBackGroundBanner.png)",
     backgroundSize: "cover",
     backgroundPosition: "center",
